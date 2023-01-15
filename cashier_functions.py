e0246a122f260a8613b78ce176bdf9950632a36e
Imam <imam0067@gmail.com>
2023-01-15 07:01:07 +0700

    reformat function and main program

--- cashier_functions.py
+++ cashier_functions.py
@@ -10,33 +10,41 @@ def add_item(items):
             price_per_item = int(input("Enter price per item: "))
             if price_per_item <= 0:
                 raise ValueError
-            items.append({
-                "item_name": item_name,
-                "n_items": n_items,
-                "price_per_item": price_per_item,
-                "total_price": n_items * price_per_item
-            })
+            items.append(
+                {
+                    "item_name": item_name,
+                    "n_items": n_items,
+                    "price_per_item": price_per_item,
+                    "total_price": n_items * price_per_item,
+                }
+            )
             print(f"Item {item_name} added.")
         except ValueError:
             print("Invalid input. Item was not added.")
-        
+
         # Ask if user wants to add another item
         add_another = input("Do you want to add another item? (y/n) ")
         if add_another.lower() != "y":
             break
 
+
 def view_items(items):
-    print("-"*40)
+    print("-" * 40)
     print("Item Name".ljust(20), "Quantity".ljust(10), "Price".ljust(10))
-    print("-"*40)
+    print("-" * 40)
     for item in items:
-        print(item["item_name"].ljust(20), str(item["n_items"]).ljust(10), str(item["total_price"]).ljust(10))
+        print(
+            item["item_name"].ljust(20),
+            str(item["n_items"]).ljust(10),
+            str(item["total_price"]).ljust(10),
+        )
+
 
 def delete_item(items):
     print("Select item to delete:")
     for i, item in enumerate(items):
         print(f"{i+1}. {item['item_name']}")
-            
+
     try:
         item_number = int(input("Enter item number: "))
         if item_number > len(items) or item_number < 1:
@@ -44,19 +52,20 @@ def delete_item(items):
     except ValueError:
         print("Invalid selection.")
         return
-    
+
     confirm = input("Are you sure you want to delete this item? (y/n) ")
     if confirm.lower() == "y":
-        items.pop(item_number-1)
+        items.pop(item_number - 1)
         print("Item deleted.")
     else:
         print("Delete cancelled.")
+
 
 def update_item(items):
     print("Select item to update:")
     for i, item in enumerate(items):
         print(f"{i+1}. {item['item_name']}")
-    
+
     try:
         item_number = int(input("Enter item number: "))
         if item_number > len(items) or item_number < 1:
@@ -64,15 +73,15 @@ def update_item(items):
     except ValueError:
         print("Invalid selection.")
         return
-    
-    item = items[item_number-1]
+
+    item = items[item_number - 1]
     print(f"Updating {item['item_name']}...")
     print("What do you want to update?")
     print("1. Item name")
     print("2. Quantity")
     print("3. Price per item")
     print("4. Cancel update")
-    
+
     try:
         update_type = int(input("Enter a number: "))
         if update_type not in [1, 2, 3, 4]:
@@ -80,7 +89,7 @@ def update_item(items):
     except ValueError:
         print("Invalid selection. Update cancelled.")
         return
-    
+
     if update_type == 1:
         old_name = item["item_name"]
         item_name = input("Enter new item name: ")
@@ -107,8 +116,9 @@ def update_item(items):
         print(f"Price per item updated from {old_price_per_item} to {price_per_item}.")
     else:
         return
-    
+
     item["total_price"] = item["n_items"] * item["price_per_item"]
+
 
 def reset_transaction(items):
     reset = input("Are you sure you want to reset the transaction? (y/n) ")
@@ -117,4 +127,3 @@ def reset_transaction(items):
         print("Transaction reset.")
     else:
         print("Reset cancelled.")
-
--- project_cashier.py
+++ project_cashier.py
@@ -1,14 +1,16 @@
 from cashier_functions import *
 
+
 class Cashier:
     """
     This is the Cashier class, which is responsible for managing a list of items and calculating the total price of an order with applicable discounts.
     The init method initializes the Cashier object with an empty list of items.
     """
+
     def __init__(self):
         self.items = []
         self.payment_status = "unpaid"
-    
+
     """
     The check_order method calculates the total price of the order by summing the total prices of all items in the order. 
     If the total price is greater than 500000, a 10% discount is applied. 
@@ -20,6 +22,7 @@ class Cashier:
     It is also check the payment status, if payment status is unpaid, the user will be prompted to pay and enter their paid amount. If paid amount is more than the final price,
     the exchange will be calculated and displayed. The payment status will be updated to paid.
     """
+
     def check_order(self):
         total_price = sum([item["total_price"] for item in self.items])
         if total_price > 500000:
@@ -31,19 +34,23 @@ class Cashier:
         else:
             discount = 0
         final_price = total_price - discount
-        
-        print("-"*40)
+
+        print("-" * 40)
         print("Item Name".ljust(20), "Quantity".ljust(10), "Price".ljust(10))
-        print("-"*40)
+        print("-" * 40)
         for item in self.items:
-            print(item["item_name"].ljust(20), str(item["n_items"]).ljust(10), str(item["total_price"]).ljust(10))
-        print("-"*40)
+            print(
+                item["item_name"].ljust(20),
+                str(item["n_items"]).ljust(10),
+                str(item["total_price"]).ljust(10),
+            )
+        print("-" * 40)
         print(f"Total price before discount: {total_price}")
         print(f"Discount: {discount}")
         print(f"Final price: {final_price}")
 
         print(f"Status: {self.payment_status}")
-    
+
         if self.payment_status == "unpaid":
             pay = input("Do you want to pay now? (y/n) ")
             if pay.lower() == "y":
@@ -54,7 +61,9 @@ class Cashier:
                             raise ValueError
                         break
                     except ValueError:
-                        print("Paid amount must be more than or equal to the final price. Try again.")
+                        print(
+                            "Paid amount must be more than or equal to the final price. Try again."
+                        )
                 exchange = paid - final_price
                 self.payment_status = "paid"
                 print(f"Exchange: {exchange}")
@@ -65,34 +74,39 @@ class Cashier:
     If the input is invalid, the item is not added. 
     The user can choose to add multiple items.
     """
+
     def add_item(self):
         add_item(self.items)
 
     """
     The view_items method prints a summary of all items in the list, including the item name, quantity, and price.
     """
+
     def view_items(self):
         view_items(self.items)
-    
+
     """
     The delete_item method allows the user to select an item from the list to delete by its number. 
     If the selection is invalid, the delete operation is cancelled.
     """
+
     def delete_item(self):
         delete_item(self.items)
-    
+
     """
     The update_item method allows the user to select an item from the list to update by its number.
     If the selection is invalid, the update operation is cancelled.
     The user is then prompted to select which aspect of the item to update: item name, quantity, or price.
     If any of the inputs are invalid, the update is cancelled.
     """
+
     def update_item(self):
         update_item(self.items)
-    
+
     """
     The reset_transaction method clears the list of items in the current transaction, effectively starting a new transaction.
     """
+
     def reset_transaction(self):
         reset_transaction(self.items)
 
@@ -101,10 +115,11 @@ class Cashier:
     including adding items, viewing items, deleting items, updating items, resetting the current transaction, and checking out. 
     The user can choose an option by entering its corresponding number.
     """
+
     def menu(self):
-        
+
         while True:
-            print("-"*40)
+            print("-" * 40)
             print("1. Add item")
             print("2. View items")
             print("3. Delete item")
@@ -112,14 +127,14 @@ class Cashier:
             print("5. Check order")
             print("6. Reset transaction")
             print("7. Quit")
-            print("-"*40)
-            
+            print("-" * 40)
+
             try:
                 selection = int(input("Enter a number: "))
             except ValueError:
                 print("Invalid selection. Try again.")
                 continue
-            
+
             if selection == 1:
                 self.add_item()
             elif selection == 2:
@@ -136,7 +151,7 @@ class Cashier:
                 break
             else:
                 print("Invalid selection. Try again.")
-    
+
 
 cashier = Cashier()
 cashier.menu()
